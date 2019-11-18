381fa7f71272a1106c43c43b30f684ea82668c31
nexus-bit <kss0097@hanmail.net>
2019-11-19 01:01:21 +0900

    what is this function?

---
+++ LearningCPP/116_my_this.cpp
@@ -0,0 +1,21 @@
+////this-> 가 하는 일 : 자기 자신을 가르침, 멤버 함수 내에서만 쓸수 있음 
+//#include<iostream>
+//using namespace std;
+//
+//class thistest {
+//	int num = 0;
+//public:
+//	thistest() {
+//		this->num = 1;
+//	}
+//	thistest(int num) {
+//		this->num = num;
+//		//num = num; - 원하는답X
+//	}
+//	void sethamsu(int num) {
+//		this->num = num;
+//	}
+//	int returnhamsu() {
+//		return this->num;
+//	}
+//};
